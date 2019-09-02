d28597b8a22fedb3125d4120e38bfe9f468721df
Dawid Dobrowolski <dawid89dobrowolski@gmail.com>
2019-09-02 22:54:51 +0200

    added tests for getCollections()

--- test/test.js
+++ test/test.js
@@ -15,14 +15,31 @@ runnerOptions = {
 
 describe('NewmanRunner()',  function(){
     describe('#setupFolders()',  function(){
-            it('should contain folders: ' + JSON.stringify(runnerOptions.folders), async function(){
-                await new ziom.NewmanRunner(runnerOptions).setupFolders();
-                for (let folder in runnerOptions.folders){
-                    let directory = await fs.readdirSync('./test/');
-                    assert(directory.includes(folder));
-                    await fs.rmdirSync(runnerOptions.folders[folder]);
-                }
-            });
+        it('should contain folders: ' + JSON.stringify(runnerOptions.folders), async function(){
+            await new ziom.NewmanRunner(runnerOptions).setupFolders();
+            for (let folder in runnerOptions.folders){
+                let directory = await fs.readdirSync('./test/');
+                assert(directory.includes(folder));
+                await fs.rmdirSync(runnerOptions.folders[folder]);
+            }
+        });
     })
+    describe('#getCollections()', function(){
+        it('should generate collections', async function(){
+            await fs.mkdirSync(runnerOptions.folders.collections, {recursive: true});
+            await fs.copyFileSync('./test/testdata/collections/yolo.postman_collection.json', './test/collections/yolo.postman_collection.json');
 
+            console.log(await fs.readdirSync('./test/collections'));
+            let collectionObjects = await new ziom.NewmanRunner(runnerOptions).getCollections();
+
+            assert(collectionObjects[0].address === './test/collections/yolo.postman_collection.json');
+            assert(collectionObjects[0].name === 'yolo');
+            assert(collectionObjects[0].folders.includes('folder1'));
+            assert(collectionObjects[0].folders.includes('folder1 Copy'));
+            assert(collectionObjects[0].folders.includes('LUZEM'));
+            
+            await fs.unlinkSync('./test/collections/yolo.postman_collection.json')
+            await fs.rmdirSync(runnerOptions.folders.collections);
+        })
+    })
 })
